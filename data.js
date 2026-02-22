f9e1e134171343859d27c527da2b825c313138f5
Marme1adka <116520778+Marme1adker@users.noreply.github.com>
2026-02-22 23:49:25 +0200

    🗑 Удалена игра: Atomic Heart Premium Edition

--- data.js
+++ data.js
@@ -466,9 +466,5 @@ async function fetchGames() {
 
   
     { title: 'Atomic Heart - Premium Edition',  short: 'Atomic Heart',  group: 'Mundfish',  img: steamImg(668580),  hasDlc: true, tags: ['Экшены', 'Приключенческие игры', 'Ролевые игры'],  opts: ['dlc', 'ru'],  source: 'steam', },
-
-  
-    { title: 'Atomic Heart Premium Edition',  short: 'Atomic Heart',  group: 'Mundfish',  img: steamImg(668580),  hasDlc: true, tags: ['Экшены', 'Приключенческие игры', 'Ролевые игры'],  opts: ['dlc', 'ru'],  source: 'steam', },
-
-  ];
+];
 }
