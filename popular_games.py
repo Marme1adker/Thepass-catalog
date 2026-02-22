6de63ee975da3199e497b45808e4d97eef5d726e
Marme1adka <116520778+Marme1adker@users.noreply.github.com>
2026-02-22 23:35:22 +0200

    Create popular_games.py

---
+++ popular_games.py
@@ -0,0 +1,125 @@
+POPULAR_GAMES = {
+
+    # ══════════════════════════════════════════════════════════════
+    # Valve
+    # ══════════════════════════════════════════════════════════════
+
+
+    # ══════════════════════════════════════════════════════════════
+    # Rockstar
+    # ══════════════════════════════════════════════════════════════
+    "gta 5":                            "https://steampass.gg/catalog/steam/1e3342bf-7486-4abe-8703-6b00f29ecaad",
+    "gta v":                            "https://steampass.gg/catalog/steam/1e3342bf-7486-4abe-8703-6b00f29ecaad",
+    "grand theft auto v":               "https://steampass.gg/catalog/steam/1e3342bf-7486-4abe-8703-6b00f29ecaad",
+    "grand theft auto 5":               "https://steampass.gg/catalog/steam/1e3342bf-7486-4abe-8703-6b00f29ecaad",
+    "gta 2":                            "https://steampass.gg/catalog/steam/a7559fb7-667d-48a7-abb1-75c1e16aaff8",
+    "gta 3":                            "https://steampass.gg/catalog/steam/e931d624-5e47-4192-b413-864392f64637",
+    "grand theft auto iii":             "https://steampass.gg/catalog/steam/ef751c3e-934f-4727-9149-d4f94c7a7d60",
+    "grand theft auto iii definitive":  "https://steampass.gg/catalog/steam/e931d624-5e47-4192-b413-864392f64637",
+    "grand theft auto vice city":       "https://steampass.gg/catalog/steam/30137e58-0d68-4a59-aed1-4a7b98719113",
+    "vice city":                        "https://steampass.gg/catalog/steam/9a944c1e-3f34-4b3e-a610-6c781e48721e",
+    "grand theft auto san andreas":     "https://steampass.gg/catalog/steam/6123f93d-e91f-473c-ae1e-d2c6fc0b3502",
+    "rdr1":                             "https://steampass.gg/catalog/steam/2326e769-9c4d-49c7-ae30-93c7d4fba8ee",
+
+    # ══════════════════════════════════════════════════════════════
+    # CD Projekt Red
+    # ══════════════════════════════════════════════════════════════
+
+
+    # ══════════════════════════════════════════════════════════════
+    # Ubisoft — Assassin's Creed
+    # ══════════════════════════════════════════════════════════════
+    "assassin's creed director's cut":  "https://steampass.gg/catalog/steam/698b0448-1952-401a-b378-dda0461fd882",
+    "assassin's creed 1":               "https://steampass.gg/catalog/steam/698b0448-1952-401a-b378-dda0461fd882",
+    "assassin's creed brotherhood":     "https://steampass.gg/catalog/steam/7701cbb7-2b0c-45a6-a0d9-6ce879b538c4",
+    "assassin's creed revelations":     "https://steampass.gg/catalog/steam/81d8de4a-10c5-418d-97d2-089c59feae3e",
+    "assassin's creed liberation":      "https://steampass.gg/catalog/steam/9ab98730-a3aa-4b96-9e8c-1074180a1c08",
+    "assassin's creed freedom cry":     "https://steampass.gg/catalog/steam/a3381de4-f1eb-419f-953a-acab494cade9",
+    "assassin's creed unity":           "https://steampass.gg/catalog/steam/f4fc2d48-3866-408b-90fc-8ae24edd3e30",
+    "assassin's creed rogue":           "https://steampass.gg/catalog/steam/7f08f37e-6e11-453b-9dad-9b9a35bd90f6",
+    "assassin's creed chronicles china":"https://steampass.gg/catalog/steam/cefabcb5-ebf5-457f-bdf8-5b9465e171cf",
+    "assassin's creed black flag":      "https://steampass.gg/catalog/steam/925d864f-7dbd-4238-a77d-ad8614f4043f",
+    "assassin's creed syndicate":       "https://steampass.gg/catalog/steam/a15a46bd-ce85-4316-ac3f-557e3f5d7df9",
+    "assassin's creed chronicles india":"https://steampass.gg/catalog/steam/eb880ddd-c604-4919-bde0-4515c9d3a53a",
+    "assassin's creed chronicles russia":"https://steampass.gg/catalog/steam/aeb6e1aa-9309-4c2f-8b1e-fb5078ee282b",
+    "assassin's creed origins":         "https://steampass.gg/catalog/steam/f725bbc2-4f92-4810-81fc-4e49f23caef3",
+    "assassin's creed odyssey":         "https://steampass.gg/catalog/steam/bcd6f45f-ae67-4799-9edc-b870f99fa02c",
+    "assassin's creed 3 remastered":    "https://steampass.gg/catalog/steam/671ad6a0-58d2-4833-aa1c-c6ce58e12253",
+    "assassin's creed valhalla":        "https://steampass.gg/catalog/steam/95d245d1-2406-49ba-aa10-c6382da5a5db",
+    "assassin's creed mirage":          "https://steampass.gg/catalog/steam/d973e56c-7ee4-4a20-8500-74af4df57ad6",
+    "assassin's creed shadows":         "https://steampass.gg/catalog/steam/e4f991d6-cc47-4dbe-8dc8-606d678c0976",
+
+    # ══════════════════════════════════════════════════════════════
+    # Bethesda
+    # ══════════════════════════════════════════════════════════════
+    "starfield":                        "https://steampass.gg/catalog/steam/6fdea82a-3fda-4c2d-99bc-0e71bbac9ede",
+
+    # ══════════════════════════════════════════════════════════════
+    # Sony / PlayStation Studios
+    # ══════════════════════════════════════════════════════════════
+    "gow":                              "https://steampass.gg/catalog/steam/cfca870d-f391-4cf0-ad24-2138980f87e6",
+    "gowr":                             "https://steampass.gg/catalog/steam/54f67f0e-f142-4418-9d04-afee7b89b9a3",
+    "god of war ragnarok digital deluxe":"https://steampass.gg/catalog/steam/54f67f0e-f142-4418-9d04-afee7b89b9a3",
+
+    # ══════════════════════════════════════════════════════════════
+    # FromSoftware
+    # ══════════════════════════════════════════════════════════════
+    "dark souls remastered":            "https://steampass.gg/catalog/steam/0203263c-1e6b-4b76-adc2-b459fbd3e673",
+    "dark souls 1":                     "https://steampass.gg/catalog/steam/0203263c-1e6b-4b76-adc2-b459fbd3e673",
+    "dark souls prepare to die":        "https://steampass.gg/catalog/steam/079eb780-c812-4f55-acd4-6f9d8b0ee1e0",
+    "dark souls 2 scholar of the first sin": "https://steampass.gg/catalog/steam/46bc98b5-9b15-4aba-af5b-dfd2048222a5",
+    "dark souls ii":                    "https://steampass.gg/catalog/steam/46bc98b5-9b15-4aba-af5b-dfd2048222a5",
+    "dark souls 3":                     "https://steampass.gg/catalog/steam/a45e068e-b394-4aa7-88d3-84e5c83f8b63",
+    "dark souls iii":                   "https://steampass.gg/catalog/steam/a45e068e-b394-4aa7-88d3-84e5c83f8b63",
+    "dark souls 3 deluxe edition":      "https://steampass.gg/catalog/steam/a45e068e-b394-4aa7-88d3-84e5c83f8b63",
+    "elden ring shadow of the erdtree": "https://steampass.gg/catalog/steam/6fe28b1f-4e4d-4a37-b93d-633efb92a047",
+    "elden ring dlc":                   "https://steampass.gg/catalog/steam/6fe28b1f-4e4d-4a37-b93d-633efb92a047",
+    "elden ring nightreign":            "https://steampass.gg/catalog/steam/b0574fec-7b8f-486e-9a0f-912969ec039c",
+    "sekiro":                           "https://steampass.gg/catalog/steam/3c3c1a72-659c-498f-a92d-c7ebcb48bbde",
+    "sekiro shadows die twice":         "https://steampass.gg/catalog/steam/3c3c1a72-659c-498f-a92d-c7ebcb48bbde",
+
+    # ══════════════════════════════════════════════════════════════
+    # EA
+    # ══════════════════════════════════════════════════════════════
+    "battlefield bad company 2":        "https://steampass.gg/catalog/steam/6581b470-9908-4cf4-a0f4-cb62f12c1bbc",
+    "battlefield hardline":             "https://steampass.gg/catalog/steam/50afd800-18c2-4c9e-b383-b0e5c8b695d2",
+    "battlefield 5":                    "https://steampass.gg/catalog/steam/3fbd5dbe-795e-41f9-917f-25cd52c4fe73",
+    "battlefield v":                    "https://steampass.gg/catalog/steam/3fbd5dbe-795e-41f9-917f-25cd52c4fe73",
+    "battlefield 1":                    "https://steampass.gg/catalog/steam/b9fa039b-d828-48aa-b95b-8cd176247ed5",
+    "battlefield 3":                    "https://steampass.gg/catalog/steam/6eadc9a7-15fa-4e27-b933-49cb59c1cbf2",
+    "battlefield 4":                    "https://steampass.gg/catalog/steam/15185677-113d-4e9e-a510-731d2a9c8c53",
+    "a way out":                        "https://steampass.gg/catalog/steam/89bf3404-b4a0-4665-bd14-3e747db7a6bd",
+    "the sims 3":                       "https://steampass.gg/catalog/steam/cf323395-c3b3-413a-add4-32de27f0055d",
+    "the sims 2 legacy collection":     "https://steampass.gg/catalog/steam/26d3eee3-8f59-4e23-8096-c0833a2c3115",
+    "the sims legacy collection":       "https://steampass.gg/catalog/steam/3a2eb7b9-3fae-4fff-9197-7478f8adde9d",
+    "the sims 4 deluxe edition":        "https://steampass.gg/catalog/steam/38d197b2-8989-4281-ad01-7608e799cca3",
+    "sims 4":                           "https://steampass.gg/catalog/steam/38d197b2-8989-4281-ad01-7608e799cca3",
+
+    # ══════════════════════════════════════════════════════════════
+    # 2K / Gearbox
+    # ══════════════════════════════════════════════════════════════
+    "new tales from the borderlands":   "https://steampass.gg/catalog/steam/9eddea21-f2c1-47b1-a97c-bbd4d15c9175",
+    "borderlands 4":                    "https://steampass.gg/catalog/steam/7df71f34-04bc-45a7-b645-0d5e479ba40c",
+    "borderlands game of the year":     "https://steampass.gg/catalog/steam/e6e47275-c275-48d5-8652-c917c3d97a91",
+    "borderlands goty enhanced":        "https://steampass.gg/catalog/steam/59352c4c-48b6-4ab6-9072-58f24c27f92e",
+    "borderlands 3 super deluxe":       "https://steampass.gg/catalog/steam/033056b8-144c-4702-a903-d68dc9fa08b5",
+    "borderlands the pre-sequel":       "https://steampass.gg/catalog/steam/caa09465-3883-44db-bddb-3d092c8da7b6",
+    "mafia the old country deluxe":     "https://steampass.gg/catalog/steam/5643d683-94b5-4502-9414-14073b6919b2",
+    "mafia classic":                    "https://steampass.gg/catalog/steam/799bd783-d8db-45b8-be01-dceebb8239d7",
+    "mafia 3 definitive edition":       "https://steampass.gg/catalog/steam/fd0411eb-3fba-423c-af86-f2a35ca4a3fc",
+    "mafia iii":                        "https://steampass.gg/catalog/steam/fd0411eb-3fba-423c-af86-f2a35ca4a3fc",
+    "the bureau xcom declassified":     "https://steampass.gg/catalog/steam/98a36fd6-c10c-41f8-a195-f6c1949c2bae",
+    "xcom enemy unknown":               "https://steampass.gg/catalog/steam/29f6b25d-8665-43b3-ada1-b05a2820ecb5",
+    "xcom 2 all dlc":                   "https://steampass.gg/catalog/steam/2043ccaa-e3de-4f8c-a8cd-ac2f60c75840",
+    "civilization vii":                 "https://steampass.gg/catalog/steam/d49f17e1-066f-4575-97b3-d4f9d15b6ee3",
+    "civ 7":                            "https://steampass.gg/catalog/steam/d49f17e1-066f-4575-97b3-d4f9d15b6ee3",
+    "civilization beyond earth":        "https://steampass.gg/catalog/steam/6a991f04-dc38-48a0-9273-188fe067360d",
+    "civilization 5 complete":          "https://steampass.gg/catalog/steam/3eda7c6f-0504-49b5-aa38-0dc328af6270",
+    "civilization vi anthology":        "https://steampass.gg/catalog/steam/cbcfad0d-810d-49d0-b7bb-4c833dd88c9b",
+
+    # ══════════════════════════════════════════════════════════════
+    # Indie / AA
+    # ══════════════════════════════════════════════════════════════
+    "subnautica":                       "https://steampass.gg/catalog/steam/c32dc8e3-1066-46ed-892b-6f37006e25b3",
+    "subnautica below zero":            "https://steampass.gg/catalog/steam/2da2ac48-a723-431b-b854-979175794031",
+}
